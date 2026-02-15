feaccc277ed662564da0f4374a2440379b279dc3
Renn Le <rennlttmy@gmail.com>
2026-02-15 04:23:31 -0800

    Set up Scanner and BREAK constant

--- chavvi-calc/src/main/java/chavvidemo/example/ChavviCalcApp.java
+++ chavvi-calc/src/main/java/chavvidemo/example/ChavviCalcApp.java
@@ -1,5 +1,7 @@
 package chavvidemo.example;
 
+import java.util.*;
+
 /**
  * Chavvi Calc - Menu Implementation
  *
@@ -9,6 +11,8 @@ public class ChavviCalcApp {
 	// Global variables:
 	private static float valA = 0.0f;
 	private static float valB = 0.0f;
+	private static final String BREAK = "------------------------------------------------";
+	private static final Scanner scanner = new Scanner(System.in);
 
 	public static void main(String[] args) {
 		System.out.println("Chavvi's Calculator:");
@@ -22,13 +26,13 @@ public class ChavviCalcApp {
 	 * Show current values of A and B and format to 3 decimals
 	 */
 	private static void printMenu() {
-		System.out.println("-----------------------------------------------");
+		System.out.println(BREAK);
 		System.out.printf("A = %.3f    B = %.3f%n", valA, valB);
-		System.out.println("-----------------------------------------------");
+		System.out.println(BREAK);
 		System.out.println("a : Enter a number for A");
 		System.out.println("b : Enter a number for B");
 		System.out.println("+ : Add");
-		System.out.println("- : Substract");
+		System.out.println("- : Subtract");
 		System.out.println("* : Multiply");
 		System.out.println("/ : Divide");
 		System.out.println("c : Clear A and B");
