885f63f5114c4c7452a046317297aeaaba446444
Renn Le <rennlttmy@gmail.com>
2026-02-15 03:20:14 -0800

    Implemented menu UI
    Added global variables valA, valB and printMenu method

--- helloworld/src/main/java/chavvidemo/example/App.java
+++ helloworld/src/main/java/chavvidemo/example/App.java
@@ -1,13 +1,38 @@
 package chavvidemo.example;
 
 /**
- * Hello world!
+ * Chavvi Calc - Menu Implementation
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+
+	// Global variables:
+	private static float valA = 0.0f;
+	private static float valB = 0.0f;
+
+	public static void main(String[] args) {
+		System.out.println("Chavvi's Calculator:");
+
+		// call menu function
+		printMenu();
+	}
+
+	/**
+	 * Print menu UI
+	 * Show current values of A and B and format to 3 decimals
+	 */
+	private static void printMenu() {
+		System.out.println("-----------------------------------------------");
+		System.out.printf("A = %.3f    B = %.3f%n", valA, valB);
+		System.out.println("-----------------------------------------------");
+		System.out.println("a : Enter a number for A");
+		System.out.println("b : Enter a number for B");
+		System.out.println("+ : Add");
+		System.out.println("- : Substract");
+		System.out.println("* : Multiply");
+		System.out.println("/ : Divide");
+		System.out.println("c : Clear A and B");
+		System.out.println("q : Quit");
+		System.out.print("Enter command: ");
+	}
 }
